847be7e016d1efadce2231e738858ddbd3229dd6
Attila Starkenius <symphtom@hotmail.com>
2022-12-17 19:59:46 +0100

    Add project files.

---
+++ Storage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Storage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Storage/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Storage.Models
+{
+    public class Product { 
+    
+        int Id { get; set; }
+        string Name { get; set; }
+    int Price { get; set; }
+    DateTime Orderdate { get; set; }
+    string Category { get; set; }
+    string Shelf { get; set; }
+    int Count { get; set; }
+    string Description { get; set; }
+}
+
+
+
